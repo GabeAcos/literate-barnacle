9b7c0589d44129b6fbf33d243f615f2643c01e14
GabeAcos <103015835+GabeAcos@users.noreply.github.com>
2025-03-26 22:56:43 -0500

    Completed Ch5:Website L3:More Pages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import *
 from htmlnode import *
+from markdown_blocks import *
 import os
 import shutil
 
@@ -35,9 +36,37 @@ def clear_public():
         os.mkdir(PUBLIC_PATH)
 
 
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    head = []
+    for line in lines:
+        if line.startswith("# "):
+            head.append(line[2:].strip())
+    return head[0]
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as f:
+        markdown_file = f.read()
+    with open(template_path, "r") as f:
+        template_file = f.read()
+
+    htmlnode = markdown_to_html_node(markdown_file)
+    content = htmlnode.to_html()
+    title = extract_title(markdown_file)
+    template_file = template_file.replace(r"{{ Content }}", content)
+    template_file = template_file.replace(r"{{ Title }}", title)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    with open(dest_path, "w") as f:
+        f.write(template_file)
+
+
 def main():
     clear_public()
     copy_static("static/")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 if __name__ == "__main__":
